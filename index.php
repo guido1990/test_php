ac4145b731766f3a73382df4a79b0e6a3a34d7a3
guido1990 <73950332+guido1990@users.noreply.github.com>
2024-05-30 19:43:28 +0200

    update db in index.php

--- index.php
+++ index.php
@@ -50,10 +50,10 @@ TR.impair {
 <body>
   <?php
    // Connexion à la base de données MariaDB
-   $server = "localhost";
+   $server = "10.0.2.4";
    $dbname = "db_ypsolap";
-   $user = "userdb";
-   $pass = "Password1234";
+   $user = "mysqladminun";
+   $pass = "mYsq1_3dFewhb";
 
    try {
    //On se connecte à la BDD
