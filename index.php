92ce27e41c9c222dab26f6409f220125cf6e9124
guido1990 <73950332+guido1990@users.noreply.github.com>
2024-05-31 14:52:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -50,10 +50,10 @@ TR.impair {
 <body>
   <?php
    // Connexion à la base de données MariaDB
-   $server = "10.0.2.4";
-   $dbname = "db_ypsolap";
-   $user = "mysqladminun";
-   $pass = "mYsq1_3dFewhb";
+   $server = getenv('SERVER');
+   $dbname = getenv('DB_NAME');
+   $user = getenv('USER');
+   $pass = getenv('PASS');
 
    try {
    //On se connecte à la BDD
